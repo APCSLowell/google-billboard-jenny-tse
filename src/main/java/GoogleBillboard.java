d6254fef7dacf306cb5530d924d10793211bf7f7
Jenny Tse <142839347+jenny-tse@users.noreply.github.com>
2024-01-22 13:02:56 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,7 +1,7 @@
 
 public void setup() {
 public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
-public static double num=0;
+public double num=0;
 public boolean primeNum = false;
 for(int i=9;i<e.length()-9 &&!primeNum;i++) {
    String substring = e.substring(i, i + 10);
