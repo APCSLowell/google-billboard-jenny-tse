fa7bc2136db47573d4428063adf849937c086308
Jenny Tse <142839347+jenny-tse@users.noreply.github.com>
2024-01-22 12:51:01 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,11 +1,10 @@
-
 public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
-
+double num=0;
 public void setup() {
 boolean primeNum = false;
 for(int i=9;i<e.length()-9 &&!primeNum;i++) {
    String substring = e.substring(i, i + 10);
-   double num = Double.parseDouble(substring);
+   num = Double.parseDouble(substring);
    if(isPrime(num)) {
       System.out.print (substring);
       primeNum = true;
